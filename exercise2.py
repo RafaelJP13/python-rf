1c83de2f9a7efbc2ae5708a65371a968aaaa47c1
Rafael Fernandes <rafaelsf013@gmail.com>
2026-08-19 18:45:55 -0300

    Add basic mathematical operations in exercise2.py

---
+++ exercise2.py
@@ -0,0 +1,12 @@
+a = 20
+b = 6
+
+print("----- Operações Matemáticas -----")
+
+print(f"Soma: {a + b}")
+print(f"Subtração: {a - b}")
+print(f"Multiplicação: {a * b}")
+print(f"Divisão: {a / b}")
+print(f"Divisão inteira: {a // b}")
+print(f"Resto da divisão: {a % b}")
+print(f"Potência: {a ** b}")

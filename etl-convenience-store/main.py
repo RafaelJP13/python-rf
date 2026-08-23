43327b78c115ee0d7ef089d3d89d7542822551f5
Rafael Fernandes <rafaelsf013@gmail.com>
2026-08-23 06:21:38 -0300

    feat: implement product extraction.

---
+++ etl-convenience-store/main.py
@@ -0,0 +1,5 @@
+from src.extract import extract
+
+products = extract()
+
+print(products)
---
+++ etl-convenience-store/src/extract.py
@@ -0,0 +1,27 @@
+from typing import TypedDict
+import csv
+
+from src.types import Product
+
+def extract() -> list[Product]:
+    with open(
+        "data/raw/products.csv",
+          newline="", 
+          encoding="utf-8"
+          ) as file:
+        products = csv.DictReader(file)
+
+        result: list[Product] = []
+
+        for product in products:
+              result.append(
+                  {
+                    "id": product["id"],
+                    "name": product["name"],
+                    "category": product["category"],
+                    "price": product["price"],
+                    "supply": product["supply"],
+                  }
+              )
+
+        return result
